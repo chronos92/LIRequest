26f368f4d7e835985868bfa651852e029bf4f133
Boris Falcinelli <boris@labinfo.net>
2016-10-17 16:02:42 +0200

    corretti metodi interni

--- LIRequest/Instance.swift
+++ LIRequest/Instance.swift
@@ -11,35 +11,35 @@ import Foundation
 public class LIRequestInstance : NSObject {
     
     /// Indica il Content-Type di default impostato nell'inizializzazione dell'oggetto LIRequest
-    var contentType : LIRequest.ContentType = .applicationJson
+    public var contentType : LIRequest.ContentType = .applicationJson
     
     /// Indica il valore della chiave di default contenente l'oggetto utile nella risposta
-    var callbackName : String = "data"
+    public var callbackName : String = "data"
     
     /// Indica i dati necessari per effettuare il login durante le richiesta
-    var loginData : LIRequest.LoginData? = nil
+    public var loginData : LIRequest.LoginData? = nil
     
     /// Indica lo User-Agent di default impostato all'inizializzazione dell'oggetto LIRequest
-    var userAgent : String? = nil
+    public var userAgent : String? = nil
     
     /// Indica se dovrè essere visibile l'indicatore di sistema dell'utilizzo della rete
-    var showNetworkActivityIndicator : Bool = true
+    public var showNetworkActivityIndicator : Bool = true
     
     /// Contiente l'oggetto richiamato quando la richiesta è terminata.
     /// Può essere sovvrascritto per ogni chiamata
-    var isCompleteObject : IsCompleteObject?
+    public var isCompleteObject : IsCompleteObject?
     
     /// Contiene l'oggetto richiamato in caso di errore della richiesta.
     /// Può essere sovvrascritto per ogni chiamata
-    var failureObject : FailureObject?
+    public var failureObject : FailureObject?
     
     /// Contiene l'oggetto richiamato in caso di successo della richiesta.
     /// Può essere sovvrascritto per ogni chiamata
-    var successObject : SuccessObject?
+    public var successObject : SuccessObject?
     
     /// Contiene l'oggetto responsabile della validazione della chiamata per il controllo del parametro success nel json ricevuto
     /// Può essere sovvrascritto per ogni chiamata
-    var validationResponseObject : ValidationResponseObject = { response in
+    public var validationResponseObject : ValidationResponseObject = { response in
         guard let object = response as? [AnyHashable:Any] else { return false }
         guard let success = object["success"] as? Bool else { return false }
         return success
@@ -47,14 +47,14 @@ public class LIRequestInstance : NSObject {
     
     /// Contiente l'oggetto richiamato durante il download o l'upload dei dati.
     /// Può essere sovvrascritto per ogni chiamata
-    var progressObject : ProgressObject?
+    public var progressObject : ProgressObject?
     
     /// Contiene l'oggetto responsabile per la conversione dei parametri durante la fase di preparazione della chiamata
-    var objectConversion : ObjectConversion?
+    public var objectConversion : ObjectConversion?
     
     /// Indica quale tipo di conversione verrà fatta per la creazione del corpo della chiamata
     /// Default : utf8
-    var encoding : String.Encoding = .utf8
+    public var encoding : String.Encoding = .utf8
     
     internal var listOfCall : [URLSessionTask] = []
     internal var requestForTask : [Int:LIRequest] = [:]
