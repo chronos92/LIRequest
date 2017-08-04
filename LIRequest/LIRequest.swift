17ef4e4d1e42ac2174b37bced3fb03befb84774f
Boris Falcinelli <boris@labinfo.net>
2017-08-04 15:37:39 +0200

    aggiornato problema label

--- LIRequest/LIRequest.swift
+++ LIRequest/LIRequest.swift
@@ -383,7 +383,7 @@ public class LIRequestError : NSError {
         public var errorDescription: String? {
             switch self {
             default:
-                return NSLocalizedString("ErrorCall", comment: "")
+                return LILocalizedString("ErrorCall", comment: "")
             }
         }
         
